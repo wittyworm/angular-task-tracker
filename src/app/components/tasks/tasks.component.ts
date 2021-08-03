2dc809cfa2e080af939f5c792a671c02b6289418
wittyworm <salgarpushkaraj26@gmail.com>
2021-08-04 00:43:36 +0530

    task service and getTask api added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import  { Task } from '../../Task'
-import  { TASKS } from '../../mock-tasks'
+import { TaskService } from 'src/app/services/task.service';
 
 
 @Component({
@@ -10,11 +10,13 @@ import  { TASKS } from '../../mock-tasks'
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe( tasks => 
+      this.tasks = tasks);
   }
 
 }
