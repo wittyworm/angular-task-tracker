f282c35fb4231c1e2a127b75185f9fbea0a89b44
wittyworm <salgarpushkaraj26@gmail.com>
2021-08-06 15:13:09 +0530

    about page routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
       this.subscription = this.uiService.onToggle().subscribe( value => 
         this.showAddTask = value  
       )
@@ -24,6 +25,10 @@ export class HeaderComponent implements OnInit {
 
   addTask(){
     this.uiService.toglleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url === route;
   }
 
 }
